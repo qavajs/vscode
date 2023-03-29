af163b9719c6fa89a0977996ed5f169b9ef9db05
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2023-03-29 13:26:06 +0300

    implemented cucumber lang server

--- src/extension.ts
+++ src/extension.ts
@@ -6,14 +6,44 @@ import templateAutocomplete from './templateAutocomplete';
 import poAutocomplete from './poAutocomplete';
 import executeCommand from './executeCommand';
 
+import { startEmbeddedServer } from '@cucumber/language-server/wasm'
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node'
+
+import { VscodeFiles } from './VscodeFiles'
+
+let client: LanguageClient
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 	executeCommand(context);
 	poAutocomplete(context);
-	templateAutocomplete(context);	
-	templateDecoration(context);
+	// templateAutocomplete(context);	
+	// templateDecoration(context);
+
+	const serverOptions: ServerOptions = async () =>
+    startEmbeddedServer(
+      __dirname,
+      () => new VscodeFiles(vscode.workspace.fs),
+      () => undefined
+    )
+
+  const clientOptions: LanguageClientOptions = {
+    // We need to list all supported languages here so that
+    // the language server is notified to reindex when a file changes
+    // https://code.visualstudio.com/docs/languages/identifiers#_known-language-identifiers
+    documentSelector: [
+      { scheme: 'file', language: 'cucumber' },
+      { scheme: 'file', language: 'javascript' },
+      { scheme: 'file', language: 'typescript' },
+    ],
+  }
+
+  client = new LanguageClient('Cucumber', 'Cucumber Language Server', serverOptions, clientOptions)
+
+  await client.start()
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() { }
+export async function deactivate() {
+	await client.stop()
+}
--- src/poAutocomplete.ts
+++ src/poAutocomplete.ts
@@ -1,20 +1,22 @@
 import * as vscode from 'vscode';
 import { getPoFiles } from './getPoFiles';
 
+const poRegexp = '(?<label>\\w+?)\\s*=\\s*(?<selector>\\$\\(.+?\\)\\)?)';
+
 export default function poAutocomplete(context: vscode.ExtensionContext) {
 	const poCompletionProvider = vscode.languages.registerCompletionItemProvider(
 		'cucumber',
 		{
 			async provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
 				try {
-					const poRegexp = '(?<label>\\w+?)\\s*=\\s*(?<selector>\\$\\(.+?\\)\\)?)';
 					const poFiles = await getPoFiles();
-					const pos = poFiles
-					.map(({path, content}) => {
-						const match = content.match(new RegExp(poRegexp, 'gmi'));
+					const pos = (await Promise.all(poFiles
+					.map(async ({path, content}) => {
+						const data = await content;
+						const match = data.match(new RegExp(poRegexp, 'gmi'));
 						const matches = match ? [...match] : [];
 						return { path, matches }
-					})
+					})))
 					.reduce((s: any[], e: any) => [...s, ...e.matches.map((line: string) => ({path: e.path, line}))], [])
 					.map(po => {
 						const match = po.line.match(new RegExp(poRegexp, 'mi'));
