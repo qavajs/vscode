d3817e2af7d22450e75649196961528cb8a7f221
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2023-04-03 18:46:02 +0300

    added memory suggestions

--- src/getMemoryFiles.ts
+++ src/getMemoryFiles.ts
@@ -2,13 +2,13 @@ import { join } from 'path';
 import { glob } from 'glob';
 import * as vscode from 'vscode';
 
-export async function getPoFiles(): Promise<any[]> {
+export async function getMemoryFiles(): Promise<any[]> {
     const config = vscode.workspace.getConfiguration('qavajs');
-    const templatesGlob: string | undefined = config.get('pageObjects');
+    const templatesGlob: string | undefined = config.get('memory');
     if (templatesGlob && vscode.workspace.workspaceFolders) {
         const cwd = vscode.workspace.workspaceFolders[0].uri.fsPath;
-        const templateFiles = await glob(templatesGlob ?? [], { cwd });
-        return Promise.all(templateFiles.map(async (file: string) => ({
+        const files = await glob(templatesGlob ?? [], { cwd });
+        return Promise.all(files.map(async (file: string) => ({
             path: file,
             content: (await vscode.workspace.fs.readFile(vscode.Uri.file(join(cwd, file)))).toString()
         })));
