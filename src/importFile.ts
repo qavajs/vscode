eeba95739eb2d0f57b871a95c1253839c17325de
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2023-06-11 11:43:32 +0300

    added ts-node import from workspace folder

--- src/importFile.ts
+++ src/importFile.ts
@@ -1,5 +1,13 @@
 import * as vscode from 'vscode';
+import { join } from 'path';
 enum FileType { TS, MJS, CJS };
+
+function getTSNodePath() {
+	if (vscode.workspace.workspaceFolders) {
+		const cwd = vscode.workspace.workspaceFolders[0].uri.path;
+		return join(cwd, 'node_modules', 'ts-node')
+	}
+}
 
 async function getFileType(filePath: string) {
 	if (filePath.includes('.ts')) return FileType.TS
@@ -12,7 +20,7 @@ export async function importFile(filePath: string) {
 	switch (await getFileType(filePath)) {
 		case FileType.CJS: return eval(`require('${filePath}');`);
 		case FileType.MJS: return eval(`import('${filePath}');`);
-		case FileType.TS: return eval(`(() => { require('ts-node').register({transpileOnly: true}); return require('${filePath}'); })()`);
+		case FileType.TS: return eval(`(() => { require('${getTSNodePath()}').register({transpileOnly: true}); return require('${filePath}'); })()`);
 		default: throw new Error('file is not supported');
 	}
 }
